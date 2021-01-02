4d8fcc9ec1a8568339c5f2b77ef2e9c97ed20ae0
Pace Willisson <pace@alum.mit.edu>
2021-01-01 20:08:15 -0500

    progress

--- kcombine.lisp
+++ kcombine.lisp
@@ -13,6 +13,7 @@
 
 (defvar *mod* (read-sexps "flat/flat.kicad_sch"))
 (defvar *top* (read-sexps "empty/empty.kicad_sch"))
+;(defvar *top* (read-sexps "flat/flat.kicad_sch"))
 
 (defun filter-mod (sch)
   (remove-if (lambda (el) 
@@ -36,7 +37,7 @@
 	    (setq val (logior #x40 (logand val #x0f))))
 	(format uuid "~2,'0x" val)))))
 
-(defun mod-to-sheet (sch)
+(defun mod-to-sheet (sch uuid)
   (let* ((page-width (* 11 25.4))
 	 (page-height (* 8.5 25.4))
 	 (sheet-width 60)
@@ -44,8 +45,7 @@
     (let ((posx (rand-in-range (* 0.50 page-width) (* 0.75 page-width)))
 	  (posy (rand-in-range (* 0.20 page-height) (* 0.80 page-height)))
 	  (width 60)
-	  (height 20)
-	  (uuid (uuid4)))
+	  (height 20))
       `(|sheet|
 	(|at| ,posx, posy)
 	(|size| ,width, height)
@@ -66,14 +66,87 @@
 	  (|justify| |left| |top|))
 	 )))))
 
+(defun get-sheet-instances (sch)
+  (let ((val (assoc '|sheet_instances| (cdr sch))))
+    (cond ((null val)
+	   (setq val (list '|sheet_instances|))
+	   (setf (cdr (last sch)) (list val))))
+    val))
+
+(defun get-symbol-instances (sch)
+  (let ((val (assoc '|symbol_instances| (cdr sch))))
+    (cond ((null val)
+	   (setq val (list (list '|symbol_instances|)))
+	   (setf (cdr (last sch)) val)))
+    val))
+
+(defun path-page (path)
+  (let ((val (assoc '|page| (cddr path))))
+    (if val
+	(parse-integer (cadr val))
+	0)))
+
+(defun max-page (sheet-instances)
+  (loop for p in (cdr sheet-instances)
+     maximize (path-page p)))
+
+(defun paths (sheet-instances)
+  (loop for p in (cdr sheet-instances)
+       collect (cadr p)))
+
+(defun add-path (sheet-instances path pagenum)
+  (setf (cdr (last sheet-instances))
+	`((|path| ,path (|page| ,(format nil "~d" pagenum))))))
+
+(defun scan (l)
+  (cond ((null l) nil)
+	((eq (caar l) '|lib_symbols|)
+	 l)
+	(t
+	 (scan (cdr l)))))
+
+(defun find-pos-for-sheets (sch)
+  (let ((pos (scan (cdr sch))))
+    (or pos (last sch))))
+
+
+(defun add-sheets ()
+  (let* ((sheet-instances (get-sheet-instances *top*))
+	 (paths (paths sheet-instances)))
+    (if (not (member "/" paths :test #'equal))
+	(add-path sheet-instances "/" 1))
+    (let ((max-page (max-page sheet-instances)))
+      (let ((sheet-page (+ max-page 1))
+	    (uuid (uuid4))
+	    )
+	(let ((sheet (mod-to-sheet *mod* uuid)))
+	  (add-path sheet-instances (format nil "/~a/" uuid) sheet-page)
+
+	  (let ((pos (find-pos-for-sheets *top*))
+		(slist (list sheet)))
+	    (setf (cdr slist) (cdr pos))
+	    (setf (cdr pos) slist)
+	    ))))))
+
+(add-sheets)
+
+
 
 (defvar mod-to-insert (filter-mod *mod*))
 
-(defun write-board (sch)
-  (let ((*readtable* *case-preserving-readtable*))
-    (print sch)
-    (format t "~%")))
+(defun sch-to-string (sch)
+  (with-output-to-string (out)
+    (let ((*readtable* *case-preserving-readtable*))
+      (prin1 sch out))))
 
-(write-board (mod-to-sheet *mod*))
+(defun write-output ()
+  (with-open-file (outf "combined/combined.kicad_sch" 
+			:direction :output
+			:if-exists :supersede)
+    (write-string (sch-to-string *top*) outf)
+    (write-char #\linefeed outf)))
+
+(write-output)
+
 
 
